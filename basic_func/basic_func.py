2e1b70dc7e2dfdb03126b237d7f0345fb5c8509e
yegyu <f2janyway@gmail.com>
2025-09-02 11:22:01 +0900

    submit

--- basic_func/basic_func.py
+++ basic_func/basic_func.py
@@ -1,26 +1,43 @@
 def add(a, b):
-    pass
+	return a + b
 
 
 def sub(a, b):
-    pass
+	return a  - b
 
 
 def mul(a, b):
-    pass
+	return a * b
 
 
 def div(a, b):
-    pass
+	if b == 0:
+		print("no divided by zero")
+	return a / b
 
 
 def power(base, pow):
-    pass
+	return base ** pow
 
 
 def square(base):
-    pass
+	return base ** 2
 
+
+#def test_greet():
+#    assert "안녕하신가 낯선자!" == greet()
+#    assert "안녕하십니까 마법사!" == greet(이름="마법사", 나이=50)
+#    assert "안녕 낯선자!" == greet(나이=4)
 
 def greet(이름="낯선자", 나이=20):
-    pass
+	if 이름 == "낯선자" and 나이!= 4:
+		return f"안녕하신가 {이름}!"
+	elif 이름 == "마법사" and 나이== 50:
+		return f"안녕하십니까 {이름}!"
+	elif 나이== 4:
+		return f"안녕 {이름}!"
+
+
+print(greet())
+print(greet(나이=4))
+print(greet(이름="마법사", 나이=50))
